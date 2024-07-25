2f3882f803efebcce6dc1a1ba52536b930c2e469
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-07-25 20:48:51 +0530

    Day 13 : Modules

---
+++ Day13/1.1addNumberModule.js
@@ -0,0 +1,7 @@
+// Create a module that exports a function to add two numbers.
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+export default addNumbers ;
---
+++ Day13/2.1multipleFunctions.js
@@ -0,0 +1,19 @@
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2
+}
+const sybTwoNumbers = (num1, num2) => {
+    return num1 - num2
+}
+const mulTwoNumbers = (num1, num2) => {
+    return num1 * num2
+}
+const divTwoNumbers = (num1, num2) => {
+    return num1 / num2
+}
+
+export {
+    addTwoNumbers,
+    sybTwoNumbers,
+    mulTwoNumbers,
+    divTwoNumbers
+}
